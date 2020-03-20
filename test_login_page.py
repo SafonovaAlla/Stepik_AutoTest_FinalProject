2154b9e3a25b05b40d2597d71ea438de2344acc4
unknown <Alla@DESKTOP-EK7SGT2.spbstu.ru>
2020-03-21 01:52:42 +0300

    MainPage injected into login_test to open login page

--- test_login_page.py
+++ test_login_page.py
@@ -1,8 +1,11 @@
 from .pages.base_page import BasePage
+from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 
 def test_guest_can_login_or_register(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page = LoginPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()  # открываем страницу
-    page.should_be_login_page()  # сразу три проверки, подтверждающте, что мы на странице "войти или зарегистрироваться"
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()  # сразу три проверки, подтверждающте, что мы на странице "войти или зарегистрироваться"
