a3a480b70469485c08bfb7054f6c89894707dfc8
unknown <Alla@DESKTOP-EK7SGT2.spbstu.ru>
2020-03-21 16:36:55 +0300

    product test parametrized with one of items marked as xfail

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,19 @@
 from .pages.product_page import ProductPage
-import time
+#import time
+import pytest
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link):
+    print("will test link [{}]".format(link))
     product_page = ProductPage(browser, link, 10)
     product_page.open()
 
@@ -12,3 +23,5 @@ def test_guest_can_add_product_to_basket(browser):
     print("product names are equal in card and message")
     product_page.should_be_basket_message_equal_to_product_price()
     print("product prices are equal in card and message")
+
+    print("test link [{}] done".format(link))
