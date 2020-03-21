249f1a9341e5d34379e8f5a4bfedb939c3139e6b
unknown <Alla@DESKTOP-EK7SGT2.spbstu.ru>
2020-03-21 15:56:58 +0300

    same test for new url

--- test_product_page.py
+++ test_product_page.py
@@ -2,7 +2,7 @@ from .pages.product_page import ProductPage
 import time
 
 def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     product_page = ProductPage(browser, link, 10)
     product_page.open()
 
